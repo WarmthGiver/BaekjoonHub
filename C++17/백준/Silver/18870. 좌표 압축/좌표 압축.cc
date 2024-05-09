2f190f51f435512b710f8478227bae0bcb45d6be
ZION <warmth.giver@gmail.com>
2024-05-09 21:56:53 +0900

    언어 버전 최신화
    C99, C++17 폴더 삭제

--- C++17/백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-#include <map>
-using namespace std;map<int,int>m;int c[1000000],n,i;int main(){scanf("%d",&n);for(i=n;i--;){scanf("%d", &c[i]);m[c[i]];}for(auto&p:m)p.second=++i;while(n--)cout<<m[c[n]]<<' ';}
