2b9823bf320582f1f5c55315eb68399f0e51c7cd
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-19 06:26:59 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C11/백준/Bronze/10807. 개수 세기/개수 세기.c
+++ C11/백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-int N, v, ns[201];
-
-int main()
-{
-	scanf("%d %d", &N, &v);
-
-	while (N--)
-	{
-		++ns[v + 100];
-
-		scanf("%d", &v);
-	}
-
-	printf("%d", ns[v + 100]);
-}
+int N,v,ns[201];main(){scanf("%d%d",&N,&v);while(N--){++ns[v+100];scanf("%d",&v);}printf("%d",ns[v+100]);}
