5e5b67182f0381af69d91fc3fc9b723ce2a66da2
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-19 09:19:28 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C11/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
+++ C11/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-
-int N, X, a;
-
-int main()
-{
-	scanf("%d %d", &N, &X);
-
-	while (N--)
-	{
-		scanf("%d", &a);
-
-		if (a < X) printf("%d ", a);
-	}
-}
+main(N,X,a){scanf("%d%d",&N,&X);while(N--){scanf("%d",&a);if(a<X)printf("%d ",a);}}
