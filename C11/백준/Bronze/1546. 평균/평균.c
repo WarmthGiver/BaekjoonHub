4e2bf29ec4a5e1e848610ccd6b7790ba1c3fe386
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-19 12:01:25 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C11/백준/Bronze/1546. 평균/평균.c
+++ C11/백준/Bronze/1546. 평균/평균.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int N, i, score, scores[1000];
+int N, i, score;
 
 float max, sum;
 
@@ -8,11 +8,11 @@ int main()
 {
 	scanf("%d", &N);
 
-	while (i < N)
+	while (i++ < N)
 	{
 		scanf("%d", &score);
 
-		scores[i++] = score;
+		sum += score;
 
 		if (score > max)
 		{
@@ -20,12 +20,5 @@ int main()
 		}
 	}
 
-	max = 1 / max * 100;
-
-	while (i-- > 0)
-	{
-		sum += scores[i] * max;
-	}
-
-	printf("%f", sum / N);
+	printf("%f", sum / N / max * 100);
 }
