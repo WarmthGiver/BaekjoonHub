c8b73c3646658e33bdf5968910b18547a4d2c7cb
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-21 10:07:36 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++20/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ C++20/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -1,2 +1,2 @@
 #include <iostream>
-char i=96;std::string S;main(){std::cin >>S;while(i++<122)std::cout<<(int)S.find(i)<<' ';}
+char i=96;std::string S;main(){std::cin>>S;while(i++<122)std::cout<<(int)S.find(i)<<' ';}
