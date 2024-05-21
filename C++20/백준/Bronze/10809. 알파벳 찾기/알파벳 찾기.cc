e65e1d6bad3aeeee4d7afbd64ec8754cbeb9bd78
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-21 10:02:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++20/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ C++20/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -1,28 +1,2 @@
 #include <iostream>
-
-using namespace std;
-
-int i, alphabets[26];
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-
-	cin.tie(0);
-
-	string S;
-
-	cin >> S;
-
-	i = S.length();
-
-	while (i--)
-	{
-		alphabets[S[i] - 97] = i + 1;
-	}
-
-	while (i++ < 25)
-	{
-		cout << alphabets[i] - 1 << ' ';
-	}
-}
+char i=96;std::string S;main(){std::cin >>S;while(i++<122)std::cout<<(int)S.find(i)<<' ';}
