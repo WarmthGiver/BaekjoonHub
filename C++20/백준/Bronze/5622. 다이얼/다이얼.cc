d358fb25403c3f71036e848697f36ee5e3b8f689
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-22 21:52:20 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++20/백준/Bronze/5622. 다이얼/다이얼.cc
+++ C++20/백준/Bronze/5622. 다이얼/다이얼.cc
@@ -1,16 +1,12 @@
 #include <iostream>
 
+int time, dial[] = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
+
 int main()
 {
-	std::ios_base::sync_with_stdio(0);
-
-	std::cin.tie(0);
-
 	std::string word;
 
 	std::cin >> word;
-
-	int time = 0, dial[] = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
 
 	for (const auto& c : word)
 	{
