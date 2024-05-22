b8845cd3f97948a170a867698091d7aff0d0ac51
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-23 06:56:28 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- C++20/백준/Silver/2563. 색종이/색종이.cc
+++ C++20/백준/Silver/2563. 색종이/색종이.cc
@@ -1,2 +1,2 @@
 #import<iostream>
-int x,y,i,j,p[100][100],a;main(){for(std::cin>>x;std::cin>>x>>y;)for(i=x+10;i-->x;)for(j=y+10;j-->y;)if(!p[i][j]++)++a;std::cout<<a;}
+int x,y,i,j,p[100][100],a;main(){for(std::cin>>x;std::cin>>x>>y;)for(i=10;i--;)for(j=10;j--;)if(!p[x+i][y+j]++)++a;std::cout<<a;}
