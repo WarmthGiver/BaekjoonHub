ea13768b24ab8a3cdfe1c6301d148a65661c0d83
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-23 06:11:16 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+
+    std::cin.tie(0);
+
+    int i, j;
+
+    char words[5][16] = { 0 };
+
+    for (i = 0; i < 5; ++i)
+    {
+        std::cin >> words[i];
+    }
+
+    for (i = 0; i < 15; ++i)
+    {
+        for (j = 0; j < 5; ++j)
+        {
+            if (words[j][i] != '\0')
+            {
+                std::cout << words[j][i];
+            }
+        }
+    }
+}
