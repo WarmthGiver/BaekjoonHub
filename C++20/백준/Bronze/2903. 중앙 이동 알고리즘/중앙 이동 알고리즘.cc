40525fda873a450de1196817bd135a09f25f376c
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-27 16:36:32 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
+++ C++20/백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -1,16 +1,2 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	cin.tie(0)->sync_with_stdio(0);
-
-	int N;
-
-	cin >> N;
-
-	N = 1 + (1 << N);
-
-	cout << N * N;
-}
+#include<iostream>
+main(int N){std::cin>>N;N=1<<N;std::cout<<++N*N;}
