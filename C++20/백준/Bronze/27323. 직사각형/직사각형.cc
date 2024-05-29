48c585b86af8e6c95d6ca0e917baae408d4dae44
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-29 23:22:39 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/27323. 직사각형/직사각형.cc
+++ C++20/백준/Bronze/27323. 직사각형/직사각형.cc
@@ -1,14 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-	cin.tie(0)->sync_with_stdio(0);
-
 	int A, B;
 
-	cin >> A >> B;
+	std::cin >> A >> B;
 
-	cout << A * B;
+	std::cout << A * B;
 }
