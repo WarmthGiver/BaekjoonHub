ab3f7899128c81525322dfa5283ce983e6b4aff0
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-05-30 21:41:38 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ C++20/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -1,13 +1,2 @@
-#include <iostream>
-
-int c[6], i;
-
-int main()
-{
-	while (i < 6)
-	{
-		std::cin >> c[i++];
-	}
-
-	std::cout << (c[0] ^ c[2] ^ c[4]) << ' ' << (c[1] ^ c[3] ^ c[5]);
-}
+#import<iostream>
+int c[6],i;main(){while(i<6)std::cin >> c[i++];std::cout<<(c[0]^c[2]^c[4])<<' '<<(c[1]^c[3]^c[5]);}
