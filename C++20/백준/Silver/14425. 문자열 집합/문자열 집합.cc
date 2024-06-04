769073a31cb6b1ed8c5c3cb4ce2605a47d4cfa61
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-05 01:28:43 +0900

    [Silver IV] Title: 문자열 집합, Time: 384 ms, Memory: 9684 KB -BaekjoonHub

--- C++20/백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ C++20/백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -1,2 +1,2 @@
 #import<bits/stdc++.h>
-using namespace std;int N,M,i;main(){cin>>N>>M;string s;map<string,int>S;for(N+=M;N--&&cin>>s;N<M?i+=S[s]:S[s]=1);cout<<i;}
+using namespace std;int N,i;main(){string s;map<string,int>S;for(cin>>N;cin>>s;N--<0?i+=S[s]:S[s]=1);cout<<i;}
