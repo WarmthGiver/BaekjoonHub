91e61d4cda18c23cd9a7b8218bc3931a86d1f6d4
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-05 07:35:46 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 164 ms, Memory: 12852 KB -BaekjoonHub

---
+++ C++20/백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+
+#include<map>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	string user, status;
+
+	map<string, string, greater<string>> accessLogs;
+
+	while (n--)
+	{
+		cin >> user >> status;
+
+		accessLogs[user] = status;
+	}
+
+	for (auto accessLog : accessLogs)
+	{
+		if (accessLog.second == "enter")
+		{
+			cout << accessLog.first << '\n';
+		}
+	}
+}
