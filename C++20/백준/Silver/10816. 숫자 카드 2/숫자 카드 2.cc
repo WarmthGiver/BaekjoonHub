71949d2ee9984aaaf527864da846f48e5d0c8b06
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-06 04:53:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 264 ms, Memory: 80144 KB -BaekjoonHub

--- C++20/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ C++20/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,36 +1,32 @@
-#include <algorithm>
-
 #include <iostream>
-
-#include <vector>
 
 using namespace std;
 
+const int CARD_MAX(10000000);
+
+int cards[CARD_MAX * 2 + 1];
+
 int main()
 {
-	cin.tie(0)->sync_with_stdio(false);
+	cin.tie(0)->sync_with_stdio(0);
 
 	int N, card;
-	
-	cin >> N;
-
-	vector<int> cards(N);
-
-	while (N--)
-	{
-		cin >> cards[N];
-	}
-
-	sort(cards.begin(), cards.end());
 
 	cin >> N;
-	
+
 	while (N--)
 	{
 		cin >> card;
 
-		auto range = equal_range(cards.begin(), cards.end(), card);
+		++cards[card + CARD_MAX];
+	}
 
-		cout << range.second - range.first << ' ';
+	cin >> N;
+
+	while (N--)
+	{
+		cin >> card;
+
+		cout << cards[card + CARD_MAX] << ' ';
 	}
 }
