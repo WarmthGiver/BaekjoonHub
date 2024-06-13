c95247b87fcbd688e79fc57f0b1c822880e4e4a1
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-14 02:32:51 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ C++20/백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,46 +1,37 @@
 #include <iostream>
 
-bool is_prime_number(int x)
-{
-	if (x <= 1)
-	{
-		return false;
-	}
-
-	if (x <= 3)
-	{
-		return true;
-	}
-
-	if (x % 2 == 0 || x % 3 == 0)
-	{
-		return false;
-	}
-	
-	for (int i = 5; i * i <= x; i += 6)
-	{
-		if (x % i == 0 || x % (i + 2) == 0)
-		{
-			return false;
-		}
-	}
-
-	return true;
-}
+#include <bitset>
 
 int main()
 {
 	std::cin.tie(0)->sync_with_stdio(0);
 
-	int M, N, x;
+	int M, N, i, j;
 
 	std::cin >> M >> N;
 
-	for (x = M - 1; ++x <= N;)
+	std::bitset<1000001> eratostheness_sieve;
+
+	eratostheness_sieve.set(1);
+
+	for (i = 2; i * i <= N; ++i)
 	{
-		if (is_prime_number(x))
+		if (eratostheness_sieve.test(i))
 		{
-			std::cout << x << '\n';
+			continue;
+		}
+
+		for (j = i; i * j <= N; ++j)
+		{
+			eratostheness_sieve.set(i * j);
+		}
+	}
+
+	for (i = M; i <= N; ++i)
+	{
+		if (!eratostheness_sieve.test(i))
+		{
+			std::cout << i << '\n';
 		}
 	}
 }
