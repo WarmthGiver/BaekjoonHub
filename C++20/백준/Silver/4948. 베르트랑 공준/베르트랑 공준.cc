0232ffc19fb523498eea31a78080dd16877c6170
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-14 14:42:30 +0900

    [Silver II] Title: 베르트랑 공준, Time: 0 ms, Memory: 3016 KB -BaekjoonHub

--- C++20/백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ C++20/백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -4,41 +4,45 @@
 
 using namespace std;
 
+const int N_MAX(123456 * 2 + 1);
+
+bitset<N_MAX> eratostheness_sieve;
+
+int primeNumberCounts[N_MAX];
+
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
 
-	const int N_MAX(246913);
-
-	bitset<N_MAX> eratostheness_sieve;
-
 	eratostheness_sieve.set(1);
 
-	for (int i = 2; i * i <= N_MAX; ++i)
+	for (int i = 2; i * i < N_MAX; ++i)
 	{
 		if (eratostheness_sieve.test(i))
 		{
 			continue;
 		}
 
-		for (int j = i * i; j <= N_MAX; j += i)
+		for (int j = i * i; j < N_MAX; j += i)
 		{
 			eratostheness_sieve.set(j);
 		}
 	}
 
-	for (int N; cin >> N, N;)
-	{
-		int count{};
+	int primeNumberCount(0);
 
-		for (int i = N * 2; i > N; --i)
+	for (int i = 1; i < N_MAX; ++i)
+	{
+		if (eratostheness_sieve.test(i) == 0)
 		{
-			if (!eratostheness_sieve.test(i))
-			{
-				++count;
-			}
+			++primeNumberCount;
 		}
 
-		cout << count << '\n';
+		primeNumberCounts[i] = primeNumberCount;
+	}
+
+	for (int N; cin >> N, N != 0;)
+	{
+		cout << primeNumberCounts[N * 2] - primeNumberCounts[N] << '\n';
 	}
 }
