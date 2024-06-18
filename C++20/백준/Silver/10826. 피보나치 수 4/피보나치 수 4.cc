4d81f93540083caea38a128bba7742c59409e7dd
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-18 17:17:49 +0900

    [Silver V] Title: 피보나치 수 4, Time: 24 ms, Memory: 2156 KB -BaekjoonHub

--- C++20/백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
+++ C++20/백준/Silver/10826. 피보나치 수 4/피보나치 수 4.cc
@@ -1,52 +1,59 @@
 #include <iostream>
 
-#include <string>
+#include <vector>
 
 using namespace std;
 
-string fibonacci(short n)
+vector<short> fibonacci(short n)
 {
-	string current{ '0' }, current_old, increment{ '1' };
+	vector<short> current{ 0 }, current_old, increment{ 1 };
 
 	while (--n >= 0)
 	{
 		current_old = current;
 
-		int current_length(current.length()), carry(0);
+		int current_size(current.size());
+		
+		bool carry(0);
 
-		for (int i = 0; i < current_length; ++i)
+		for (int i = 0; i < current_size; ++i)
 		{
-			int sum = carry + current[i] + increment[i] - 96;
+			current[i] += increment[i] + carry;
 
-			carry = sum >= 10;
+			carry = current[i] >= 10;
 
-			current[i] = 48 + sum % 10;
+			current[i] %= 10;
 		}
 
 		increment = current_old;
 
 		if (carry)
 		{
-			current.reserve(current_length + 1);
+			current.reserve(current_size + 1);
 
-			current.push_back('1');
+			current.push_back(1);
 
-			increment.reserve(current_length + 1);
+			increment.reserve(current_size + 1);
 
-			increment.push_back('0');
+			increment.push_back(0);
 		};
 	}
-
-	reverse(current.begin(), current.end());
 
 	return current;
 }
 
 int main()
 {
+	cin.tie(0)->sync_with_stdio(0);
+
 	short N;
 
 	cin >> N;
 
-	cout << fibonacci(N);
+	vector<short> fibonacci_N = fibonacci(N);
+
+	for (int i = fibonacci_N.size(); --i >= 0;)
+	{
+		cout << fibonacci_N[i];
+	}
 }
