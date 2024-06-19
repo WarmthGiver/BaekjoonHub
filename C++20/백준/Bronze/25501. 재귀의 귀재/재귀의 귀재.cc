8e45074d32d4dba039e0228e9763ed27f2e33d0a
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-06-19 23:44:56 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- C++20/백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ C++20/백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,31 +1,6 @@
 #include <iostream>
 
-#include <string.h>
-
-using namespace std;
-
-pair<bool, int> recursion(const char *s, int l, int r)
-{
-	if (l >= r)
-	{
-		return { 1, l + 1 };
-	}
-
-	else if (s[l] != s[r])
-	{
-		return { 0, l + 1 };
-	}
-
-	else
-	{
-		return recursion(s, l + 1, r - 1);
-	}
-}
-
-pair<bool, int> isPalindrome(const char *s)
-{
-	return recursion(s, 0, strlen(s) - 1);
-}
+#include <cstring>
 
 int main()
 {
@@ -39,10 +14,12 @@ int main()
 	{
 		char S[1001];
 
-		cin >> S;
+		std::cin >> S;
 
-		auto result = isPalindrome(S);
+		int i(0), j(strlen(S) - 1);
 
-		cout << result.first << ' ' << result.second << '\n';
+		for (; i < j && S[i] == S[j]; ++i, --j);
+
+		std::cout << (i >= j) << ' ' << i + 1 << '\n';
 	}
 }
