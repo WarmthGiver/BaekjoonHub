ea1bea7ffe452071c4b3d313e80e73f9c6e53eca
ZION <77770090+WarmthGiver@users.noreply.github.com>
2024-09-02 23:49:51 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.cpp
@@ -8,14 +8,14 @@ int solution(int h1, int m1, int s1, int h2, int m2, int s2)
     
     int answer = end * 59 / 3600 + end * 719 / 43200 - start * 59 / 3600 - start * 719 / 43200;
     
-    if (start >= 43200)
-    {
-        ++answer;
-    }
-    
     if (end >= 43200)
     {
         --answer;
+        
+        if (start >= 43200)
+        {
+            ++answer;
+        }
     }
     
     if (start % 3600 == 0)
